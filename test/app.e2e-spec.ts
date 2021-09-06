e37c7f67c5fa977cc8bb91e2756d747487130bca
SeaFalcon <seafalcon323@gmail.com>
2021-09-07 02:19:17 +0900

    Implement e2e-test createPodcast, getAllPodcasts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,11 +2,21 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { getConnection, Repository } from 'typeorm';
 import { Podcast } from 'src/podcast/entities/podcast.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
 const GRAPHQL_ENDPOINT = '/graphql';
+
+const testPodcast = {
+  title: 'how to build nest.js backend',
+  category: 'programming',
+};
+
+const testEpisode = {
+  title: 'app.module',
+  category: 'initialize',
+};
 describe('App (e2e)', () => {
   let app: INestApplication;
   let podcastsRepository: Repository<Podcast>;
@@ -23,7 +33,75 @@ describe('App (e2e)', () => {
     await app.init();
   });
 
+  afterAll(() => {
+    getConnection().dropDatabase();
+  });
+
   describe('Podcasts Resolver', () => {
+    describe('createPodcast', () => {
+      it('should create podcast', () => {
+        return request(app.getHttpServer())
+          .post(GRAPHQL_ENDPOINT)
+          .send({
+            query: `
+            mutation {
+              createPodcast(input: {
+                title: "${testPodcast.title}"
+                category: "${testPodcast.category}"
+              }){
+                ok
+                error
+                id
+              }
+            }`,
+          })
+          .expect(200)
+          .expect(res => {
+            const {
+              body: {
+                data: {
+                  createPodcast: { id, ok, error },
+                },
+              },
+            } = res;
+
+            expect(id).toBe(1);
+            expect(ok).toBeTruthy();
+            expect(error).toBeNull();
+          });
+      });
+
+      it('should fail if podcast input value is wrong', () => {
+        return request(app.getHttpServer())
+          .post(GRAPHQL_ENDPOINT)
+          .send({
+            query: `
+            mutation {
+              createPodcast(input: {
+                title: 1
+                category: "${testPodcast.category}"
+              }){
+                ok
+                error
+                id
+              }
+            }`,
+          })
+          .expect(400)
+          .expect(res => {
+            const {
+              body: { errors },
+            } = res;
+
+            const [error] = errors;
+
+            expect(error.message).toBe(
+              'String cannot represent a non string value: 1',
+            );
+          });
+      });
+    });
+
     describe('getAllPodcasts', () => {
       it('should return all podcasts', () => {
         return request(app.getHttpServer())
@@ -35,21 +113,36 @@ describe('App (e2e)', () => {
                 ok
                 error
                 podcasts {
+                  id
                   title
+                  category
                 }
               }
             }`,
           })
-          .expect(200);
+          .expect(200)
+          .expect(res => {
+            const {
+              body: {
+                data: {
+                  getAllPodcasts: { ok, error, podcasts },
+                },
+              },
+            } = res;
+
+            expect(podcasts).toHaveLength(1);
+            expect(ok).toBeTruthy();
+            expect(error).toBeNull();
+          });
       });
     });
 
     it.todo('getPodcast');
-    it.todo('getEpisodes');
-    it.todo('createPodcast');
-    it.todo('deletePodcast');
     it.todo('updatePodcast');
+    it.todo('deletePodcast');
+
     it.todo('createEpisode');
+    it.todo('getEpisodes');
     it.todo('updateEpisode');
     it.todo('deleteEpisode');
   });
